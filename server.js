d962f3152519b87ee8e259687eabe0e01278efce
Sathya <sathya_anisetti@brown.edu>
2018-04-19 20:06:20 -0400

    merged my work with James's work

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var anyDB = require('any-db');
 var bcrypt = require('bcrypt-nodejs');
- 
+
 
 var conn = anyDB.createConnection('sqlite3://freespeed.db');
-conn.query('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, permission INTEGER, firstName TEXT, lastName TEXT)');
-conn.query('CREATE TABLE IF NOT EXISTS boats (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, size INTEGER)');
-conn.query('CREATE TABLE IF NOT EXISTS workouts (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, type TEXT)');
-conn.query('CREATE TABLE IF NOT EXISTS workoutUserBoat (id INTEGER PRIMARY KEY AUTOINCREMENT, workoutID INTEGER, userID INTEGER, boatID INTEGER, startTime TEXT)');
-conn.query('CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY AUTOINCREMENT, workoutUserBoatID INTEGER, interval INTEGER, distanceGPS REAL, distanceIMP REAL, elapsedTime TEXT, splitGPS TEXT, speedGPS REAL, splitIMP REAL, speedIMP REAL, strokeRate REAL, totalStrokes INTEGER, distancePerStrokeGPS REAL,distancePerStrokeIMP REAL, heartRateBPM INTEGER, power INTEGER, catch INTEGER, slip INTEGER, finish INTEGER, wash INTEGER, forceAvg INTEGER, work INTEGER, forceMax INTEGER, maxForceAngle INTEGER, GPSLat REAL, GPSLon REAL)')
+conn.query('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL, permission INTEGER NOT NULL, firstName TEXT NOT NULL, lastName TEXT NOT NULL)');
+conn.query('CREATE TABLE IF NOT EXISTS boats (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL UNIQUE, size INTEGER NOT NULL)');
+/* NOTE: BOATS TABLE SHOULD BE PRE-POPULATED*/
+conn.query('CREATE TABLE IF NOT EXISTS workouts (id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT NOT NULL, type TEXT NOT NULL)');
+conn.query('CREATE TABLE IF NOT EXISTS workoutUserBoat (id INTEGER PRIMARY KEY AUTOINCREMENT, workoutID INTEGER NOT NULL, username TEXT NOT NULL, boatID INTEGER NOT NULL, startTime TEXT NOT NULL)');
+conn.query('CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY AUTOINCREMENT, workoutUserBoatID INTEGER NOT NULL, interval INTEGER, distanceGPS REAL, distanceIMP REAL, elapsedTime TEXT, splitGPS TEXT, speedGPS REAL, splitIMP REAL, speedIMP REAL, strokeRate REAL, totalStrokes INTEGER, distancePerStrokeGPS REAL,distancePerStrokeIMP REAL, heartRateBPM INTEGER, power INTEGER, catch INTEGER, slip INTEGER, finish INTEGER, wash INTEGER, forceAvg INTEGER, work INTEGER, forceMax INTEGER, maxForceAngle INTEGER, GPSLat REAL, GPSLon REAL)');
 
 var engines = require('consolidate');
 var colors = require('colors');
@@ -53,20 +54,116 @@ app.get('/main/rower/:rowerUsername', function(request, response){
 	response.render('home.html', {username: request.params.rowerUsername});
 })
 
-app.post('/personal-data-page', function(request, response) {
+app.get('/personal-data-page', function(request, response) {
+  // 1. Authenticate user is allowed to make this post
+  // 2. fetch most recent workout of the user and give them all data
   console.log('- Request received:', request.method.cyan, request.url.underline);
   response.sendFile('public/personal_page.html', {root: __dirname });
+});
+
+
+app.get('/upload-data', function(request, response) {
+  console.log('- Request received:', request.method.cyan, request.url.underline);
+  response.sendFile('public/upload-data.html', {root: __dirname });
 });
 
 app.post('/data-upload', function(request, response) {
-
+  // 1. Authenticate user is allowed to do what they did using JWT
+  // 2. Authenticate that data uploaded is valid
+  // 3. Upload data to database
+  var data = request.body;
+  var code = Number(request.body.code);
+  if (code === 0) {
+    // Create New Workout + New boat
+    createNewWorkout(request.body);
+  } else if (code === 1) {
+    // Create New Boat for existing workout
+    createNewBoatUsers(request.body);
+  } else if (code === 2) {
+    //TODO: Update Existing Entry
+  }
   console.log('- Request received:', request.method.cyan, request.url.underline);
-  response.sendFile('public/personal_page.html', {root: __dirname });
 });
+
+/*
+ * This function creates a new workout and edits the workoutId of the request's
+ * JSON file. It then calls createNewBoat()
+ */
+function createNewWorkout(data) {
+  var sql = "INSERT INTO workouts (date, type) VALUES (?, ?)";
+  conn.query(sql, [data.workoutDate, data.workoutType], function (err, row) {
+    if (err === null) {
+      data.workoutID = row.lastInsertId;
+      for (var i = 0; i < data.numUsers; i++) {
+        var username = data[makeJSON(i, "username", -1)];
+        var startTime = data[makeJSON(i, "startTime", -1)];
+        var numRows = data[makeJSON(i, "numberRows", -1)];
+        createNewWorkoutUserBoat(i, startTime, username, data, numRows);
+      }
+    } else {
+      /*TODO: Handle Error */
+      console.log(err);
+    }
+  });
+}
+
+/*
+ * This function expects the data to contain the corresponding workout id and adds
+ * data to the workoutUserBoat table.
+ */
+function createNewWorkoutUserBoat(currUserInd, startTime, username, data, numRows) {
+  var sql = "INSERT INTO workoutUserBoat (workoutID, username, boatID, startTime) VALUES (?, ?, ?, ?)";
+  conn.query(sql, [data.workoutID, username, data.boatID, startTime], function (err, row) {
+    if (err === null) {
+      console.log(String(username) + " has new workout data!");
+      data.workoutUserBoatID = row.lastInsertId;
+      insertData(currUserInd, data, numRows);
+    } else {
+      /*TODO: Handle Error*/
+      console.log(err);
+    }
+  });
+}
+
+function insertData(currUserInd, data, numRows) {
+  var toInsert = [];
+  var sql = "INSERT INTO data (workoutUserBoatID, interval, distanceGPS, distanceIMP, elapsedTime, splitGPS, speedGPS, splitIMP, speedIMP, strokeRate, totalStrokes, distancePerStrokeGPS, distancePerStrokeIMP, heartRateBPM, power, catch, slip, finish, wash, forceAvg, work, forceMax, maxForceAngle, GPSLat, GPSLon)" +
+  " VALUES ";
+  for (var i = 0; i < numRows; i++) {
+    sql += "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?), ";
+    toInsert.push(data.workoutUserBoatID);
+    var concatArray = data[makeJSON(currUserInd, "per-stroke-data", i)];
+    for (var j = 0; j < 24; j++) {
+      if (j != 3 & j != 4 & j != 6) {
+        concatArray[j] = Number(concatArray[j]);
+      }
+    }
+    toInsert = toInsert.concat(concatArray);
+  }
+  sql = sql.slice(0, -2);
+  conn.query(sql, toInsert, function(err, res) {
+    if (err === null) {
+      console.log("Records succesfully added");
+    } else {
+      /*TODO: Handle Error*/
+      console.log(err);
+    }
+  });
+}
+
+function makeJSON(firstIndex, secondIndex, thirdIndex) {
+  // if thirdIndex = -1;
+  var string = "users[" + String(firstIndex) + "][" + String(secondIndex) + "]";
+  if (thirdIndex != -1) {
+    string += "[" + String(thirdIndex) +"][]";
+  }
+
+  return string;
+}
 
 app.post('/validate-login-credetials', function(request, response){
 	var username = escape(request.body.username);
-	var password = escape(request.body.password); 
+	var password = escape(request.body.password);
 	conn.query('SELECT * FROM users WHERE username=$1', [username], function(error, result){
 		if(error){
 			console.log(error);
@@ -74,21 +171,21 @@ app.post('/validate-login-credetials', function(request, response){
 			if(result.rows.length == 0){
 				io.to(request.body.socketID).emit('usernameNotFound', {});
 			}else{
-				var hashedPassword = result.rows[0].password; 
+				var hashedPassword = result.rows[0].password;
 				bcrypt.compare(password, hashedPassword, function(err, res) {
    					 if(res){
    					 	//password was correct
    					 	var permission = result.rows[0].permission;
    					 	if(permission == 1){
    					 		//verified coach
-   					 		//eventually attach a token here 
+   					 		//eventually attach a token here
    					 		io.to(request.body.socketID).emit('validCredentials', {permission: 1, username: username});
    					 		//response.redirect('/main/coach/' + username);
    					 		//io.to(request.body.socketID).emit('usernameNotFoun', {});
    					 	}else{
    					 		//verified rower
-   					 		//eventually attach a token here 
-   					 		io.to(request.body.socketID).emit('validCredentials', {permission: 0, username: username}); 
+   					 		//eventually attach a token here
+   					 		io.to(request.body.socketID).emit('validCredentials', {permission: 0, username: username});
    					 	}
    					 }else{
    					 	io.to(request.body.socketID).emit('incorrectPassword', {});
@@ -102,8 +199,8 @@ app.post('/validate-login-credetials', function(request, response){
 app.post('/add-new-user', function(request, response) {
 	var username = escape(request.body.username);
 	var firstName = escape(request.body.firstname);
-	var lastName = escape(request.body.lastname); 
-	var password = escape(request.body.password); 
+	var lastName = escape(request.body.lastname);
+	var password = escape(request.body.password);
 	var confirmPassword = escape(request.body.confirmPassword);
 	var permission = request.body.permission;
 	console.log(permission)
@@ -144,7 +241,7 @@ app.post('/add-new-user', function(request, response) {
 								}
 							})
 						}
-					}); 
+					});
 				}
 			}
 		})
@@ -152,7 +249,7 @@ app.post('/add-new-user', function(request, response) {
 })
 
 
-/// socket events 
+/// socket events
 
 io.on('connection', function(socket){
 
